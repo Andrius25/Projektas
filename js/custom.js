a802fd2fa00f8807772790ed91bd45f6a482a030
andrius <andriuxb17@gmail.com>
2022-06-06 15:50:48 +0300

    papildytas

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
     autoplay:true,
     autoplayTimeout:3000,
     autoplayHoverPause:true,
+    responsiveclass:true,
     responsive:{
         0:{
             items:1
@@ -17,3 +17,30 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+var modal1 = document.getElementById("prisijungti");
+var modal2 = document.getElementById("registruotis");
+
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+
+var span1 = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+span1.onclick = function() {
+    modal1.style.display = "none";    
+} 
+span2.onclick = function(){
+    modal2.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal2||event.target == modal1) {
+      modal2.style.display = "none";
+      modal1.style.display = "none";
+    }
+} 
